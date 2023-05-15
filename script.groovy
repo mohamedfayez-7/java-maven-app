9fe153e6594bfe0405c5bea01a6567531fefbeef
Mohamed Fayez <mohamedfayez_7@yahoo.com>
2023-05-15 08:38:28 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,16 +1,19 @@
-def buildApp() {
-    echo 'building the application...'
+def buildJar() {
+    echo "building the application..."
+    sh 'mvn package'
 } 
 
-def testApp() {
-    echo 'testing the applicatopn...'
-    echo 'doma'
-    echo 'koko'
-}
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t mohamedfayez7/my-repo:jma-2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push mohamedfayez7/my-repo:jma-2.0'
+    }
+} 
 
 def deployApp() {
     echo 'deploying the application...'
-    echo "deploying version ${params.VERSION}"
 } 
 
 return this
